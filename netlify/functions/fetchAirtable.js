19f1ff735f8db97901feb8ce52592afa61bcdbf1
mihailooo <mihailothemix@gmail.com>
2025-09-02 22:09:38 +0200

    created function

---
+++ netlify/functions/fetchAirtable.js
@@ -0,0 +1,37 @@
+export const handler = async () => {
+  const API_KEY =
+    "pathxuJGghrYvGJj4.61ae89c2cb92e985809f20fd96bd79d35b2b6590fb63f97059f04f4f6bd2fc03";
+  const URL = "https://api.airtable.com/v0";
+
+  const BASE_ID = "appEgHRWQsvF5F7pL/tblChiOS40d8z752f";
+  const TABLE_NAME = "tblChiOS40d8z752f";
+
+  const getImages = `${URL}/${BASE_ID}/${TABLE_NAME}`;
+
+  const response = await fetch(getImages, {
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+    },
+  });
+  const data = await response.json();
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+    body: JSON.stringify({
+      data,
+    }),
+  };
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const fetchBtn = document.getElementById("fetch-btn");
+  const responseText = document.getElementById("response-output");
+
+  fetchBtn.addEventListener("click", () => {
+    handler();
+  });
+});
