7ab5574f937abf9131d7506aba58f9507f98a514
mihailooo <mihailothemix@gmail.com>
2025-09-03 02:54:10 +0200

    storeData

---
+++ netlify/functions/storeData.js
@@ -0,0 +1,68 @@
+import fetch from "node-fetch";
+import PDFDocument from "pdfkit";
+import fs from "fs";
+
+export const handler = async (event) => {
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      },
+      body: "OK",
+    };
+  }
+
+  const API_KEY =
+    "pathxuJGghrYvGJj4.61ae89c2cb92e985809f20fd96bd79d35b2b6590fb63f97059f04f4f6bd2fc03";
+  const URL = "https://api.airtable.com/v0";
+
+  const BASE_ID = "appEgHRWQsvF5F7pL";
+  const TABLE_NAME = "tbl5cmdonx7I3JxZK";
+
+  const fullUrl = `${URL}/${BASE_ID}/${TABLE_NAME}`;
+
+  const images = event.body;
+
+  const doc = new PDFDocument({ autoFirstPage: false });
+  const chunks = [];
+
+  doc.on("data", (chunk) => chunks.push(chunk));
+  doc.on("end", () => {});
+
+  images.forEach((img) => {
+    doc.addPage();
+    doc.image(img, {
+      fit: [400, 400],
+      align: "center",
+      valign: "center",
+    });
+  });
+
+  const pdfBuffer = await new Promise((resolve) => {
+    doc.on("end", () => resolve(Buffer.concat(chunks)));
+  });
+
+  const response = await fetch(fullUrl, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+    },
+  });
+  const data = await response.json();
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+    body: (() => {
+      const form = new FormData();
+      form.append("file", pdfBuffer, "test.pdf");
+      return form;
+    })(),
+  };
+};
