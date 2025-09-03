4155f585498663ead5d3c1c7475ae162fc7607af
mihailooo <mihailothemix@gmail.com>
2025-09-03 03:19:14 +0200

    try

--- netlify/functions/storeData.js
+++ netlify/functions/storeData.js
@@ -24,48 +24,61 @@ export const handler = async (event) => {
 
   const fullUrl = `${URL}/${BASE_ID}/${TABLE_NAME}`;
 
-  const { images } = JSON.parse(event.body);
+  try {
+    const { images } = JSON.parse(event.body);
 
-  const doc = new PDFDocument({ autoFirstPage: false });
-  const chunks = [];
+    const doc = new PDFDocument({ autoFirstPage: false });
+    const chunks = [];
 
-  doc.on("data", (chunk) => chunks.push(chunk));
-  doc.on("end", () => {});
+    doc.on("data", (chunk) => chunks.push(chunk));
+    doc.on("end", () => {});
 
-  images.forEach((img) => {
-    doc.addPage();
-    doc.image(img, {
-      fit: [400, 400],
-      align: "center",
-      valign: "center",
+    images.forEach((img) => {
+      doc.addPage();
+      doc.image(img, {
+        fit: [400, 400],
+        align: "center",
+        valign: "center",
+      });
     });
-  });
-  doc.end();
+    doc.end();
 
-  const pdfBuffer = await new Promise((resolve) => {
-    doc.on("end", () => resolve(Buffer.concat(chunks)));
-  });
+    const pdfBuffer = await new Promise((resolve) => {
+      doc.on("end", () => resolve(Buffer.concat(chunks)));
+    });
 
-  const form = new FormData();
-  form.append("fields[GeneratedPDF]", pdfBuffer, { filename: "output.pdf" });
+    const form = new FormData();
+    form.append("fields[GeneratedPDF]", pdfBuffer, { filename: "output.pdf" });
 
-  const response = await fetch(fullUrl, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${API_KEY}`,
-    },
-    body: form,
-  });
-  const data = await response.json();
+    const response = await fetch(fullUrl, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+      },
+      body: form,
+    });
+    const data = await response.json();
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ success: true, airtableResponse: data }),
-  };
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ success: true, airtableResponse: data }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ success: false, error: error }),
+    };
+  }
 };
